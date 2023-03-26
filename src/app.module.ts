7b7e89d4c1007b1106a8c6739dbf56c93e55a64f
hishamac <122851090+hishamac@users.noreply.github.com>
2023-03-26 08:55:47 +0000

    saved

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BlogModule } from './blog/blog.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    BlogModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ src/blog/blog.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Blog } from './blog.entity';
+import { BlogInput } from './dtos/create-blog.input';
+
+@Injectable()
+export class BlogService {
+    constructor(
+        @InjectRepository(Blog)
+        private blogRepository: Repository<Blog>,
+    ) { }
+
+    async createBlog(blogInput: BlogInput): Promise<Blog> {
+        const newBlog = this.blogRepository.create(blogInput);
+
+        return this.blogRepository.save(newBlog);
+    }
+
+    async getAll(): Promise<Blog[]> {
+        return await this.blogRepository.find();
+    }
+}
+
