d78e9d125ae8fe9768eeb4a3642d0c68889e3af1
hishamac <122851090+hishamac@users.noreply.github.com>
2023-03-27 05:07:11 +0000

    saved

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,10 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: ':memory:',
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    BlogModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
